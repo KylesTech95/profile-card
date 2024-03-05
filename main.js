d75b89869271e9ccaa26adecab5e6055e76e7e23
kylestech95 <kylestech95@outlook.com>
2024-03-04 22:16:00 -0500

    feat: javascript

--- main.js
+++ main.js
@@ -3,3 +3,21 @@ let inputs = document.querySelectorAll('input')
 inputs = [...inputs].slice(1);
 let arr = [photo].concat(inputs)
 console.log(arr)
+arr=[...arr].reverse()
+
+const appearTime =(arr,i,outcome)=>{
+    setTimeout(()=>{
+        arr[i].style=outcome;
+    }, 95*(i+1))
+}
+console.log(photo)
+const inputAppearFn = (arr) => {
+for(let i = arr.length-1; i >=0; i--){
+    const showing = `opacity:1;`
+    setTimeout(()=>{
+        appearTime(arr,i,showing)
+    },1000)
+    
+}
+}
+inputAppearFn(arr)
