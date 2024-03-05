c4af23d16b81524f4544d0ad6c08e0a9e667b5b3
kylestech95 <kylestech95@outlook.com>
2024-03-04 22:32:46 -0500

    test: animate

--- main.js
+++ main.js
@@ -6,17 +6,22 @@ console.log(arr)
 arr=[...arr].reverse()
 
 const appearTime =(arr,i,outcome)=>{
+    const l = arr[i].labels;
+console.log(l)
     setTimeout(()=>{
         arr[i].style=outcome;
+        if(l.length > 0){
+            arr[i].labels[0].style=outcome
+        }
     }, 95*(i+1))
 }
 console.log(photo)
 const inputAppearFn = (arr) => {
-for(let i = arr.length-1; i >=0; i--){
+for(let i = arr.length-1; i >=1; i--){
     const showing = `opacity:1;`
-    setTimeout(()=>{
-        appearTime(arr,i,showing)
-    },1000)
+        setTimeout(()=>{
+            appearTime(arr,i,showing)
+        },1000)
     
 }
 }
